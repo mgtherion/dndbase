b28ada065e9bc378ec3e8e757259cd23f116bfc2
mgtherion <mgtherion@gmail.com>
2018-01-03 05:45:05 +0200

    messages component inplemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
