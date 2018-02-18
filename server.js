e160230fc1e50cca933a867a54b87e8bfadf2309
mgtherion <mgtherion@gmail.com>
2018-02-19 01:03:37 +0200

    i"m too old for this shit

--- server.js
+++ server.js
@@ -407,10 +407,8 @@ app.delete('/api/enchantments/:id', function(req, res) {
 
 
 app.get('/api/search', function(req, res) {
-    let query = req.body.query;
+    let query = req.query.query;
 
-    handleError(res, req.query, req.query);
-    /*
     db.collection(RACES_COLLECTION).ensureIndex({name: 'text', description: 'text'});
 
     db.collection(RACES_COLLECTION)
@@ -422,5 +420,4 @@ app.get('/api/search', function(req, res) {
                 res.status(200).json(docs);
             }
         });
-    */
 });
