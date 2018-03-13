5f82d4005d4b3a36884c91c8ec08d64b489c1db5
mgtherion <mgtherion@gmail.com>
2018-03-13 14:10:54 +0200

    races api separated from main server file

---
+++ server/races.js
@@ -0,0 +1,76 @@
+
+module.exports = function(app, collection) {
+
+  /* '/api/races'
+   *  GET: get all races
+   *  POST: create a new race
+   */
+
+  app.get('/api/races', function(req, res) {
+      db.collection(collection).find({}).toArray(function(err, docs) {
+          if (err) {
+              handleError(res, err.message, 'Failed to get races');
+          } else {
+              res.status(200).json(docs);
+          }
+      });
+  });
+
+  app.post('/api/races', function(req, res) {
+      let newRace = req.body;
+      newRace.createDate = new Date();
+
+      if (!req.body.name) {
+          handleError(res, 'Invalid race input', 'Must provide a name', 400);
+      }
+
+      db.collection(collection).insertOne(newRace, function(err, doc) {
+          if (err) {
+              handleError(res, err.message, 'Failed to create new race');
+          } else {
+              res.status(201).json(doc.ops[0]);
+          }
+      });
+  });
+
+  /* '/api/races/:id'
+   *  GET: get race by id
+   *  PUT: update race by id
+   *  DELETE: delete race by id
+   */
+
+  app.get('/api/races/:id', function(req, res) {
+      db.collection(collection).findOne({ _id: new ObjectID(req.params.id)}, function(err, doc) {
+          if (err) {
+              handleError(res, err.message, 'Failed to get race');
+          } else {
+              res.status(200).json(doc);
+          }
+      });
+  });
+
+  app.put('/api/races/:id', function(req, res) {
+      var updateDoc = req.body;
+      delete updateDoc._id;
+
+      db.collection(collection).updateOne({ _id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+          if (err) {
+              handleError(res, err.message, 'Failed to update race');
+          } else {
+              updateDoc._id = req.params.id;
+              res.status(200).json(updateDoc);
+          }
+      });
+  });
+
+  app.delete('/api/races/:id', function(req, res) {
+      db.collection(collection).deleteOne({ _id: new ObjectID(req.params.id)}, function(err, doc) {
+          if (err) {
+              handleError(res, err.message, 'Failed to delete race');
+          } else {
+              res.status(200).json(req.params.id);
+          }
+      });
+  });
+
+}
--- server/server.js
+++ server/server.js
@@ -36,80 +36,8 @@ function handleError(res, reason, message, code) {
     res.status(code || 500).json({'error': message});
 }
 
-/* '/api/races'
- *  GET: get all races
- *  POST: create a new race
- */
 
-app.get('/api/races', function(req, res) {
-    db.collection(RACES_COLLECTION).find({}).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, 'Failed to get races');
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
-
-app.post('/api/races', function(req, res) {
-    let newRace = req.body;
-    newRace.createDate = new Date();
-
-    if (!req.body.name) {
-        handleError(res, 'Invalid race input', 'Must provide a name', 400);
-    }
-
-    db.collection(RACES_COLLECTION).insertOne(newRace, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, 'Failed to create new race');
-        } else {
-            res.status(201).json(doc.ops[0]);
-        }
-    });
-});
-
-/* '/api/races/:id'
- *  GET: get race by id
- *  PUT: update race by id
- *  DELETE: delete race by id
- */
-
-app.get('/api/races/:id', function(req, res) {
-    db.collection(RACES_COLLECTION).findOne({ _id: new ObjectID(req.params.id)}, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, 'Failed to get race');
-        } else {
-            res.status(200).json(doc);
-        }
-    });
-});
-
-app.put('/api/races/:id', function(req, res) {
-    var updateDoc = req.body;
-    delete updateDoc._id;
-
-    db.collection(RACES_COLLECTION).updateOne({ _id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, 'Failed to update race');
-        } else {
-            updateDoc._id = req.params.id;
-            res.status(200).json(updateDoc);
-        }
-    });
-});
-
-app.delete('/api/races/:id', function(req, res) {
-    db.collection(RACES_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id)}, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, 'Failed to delete race');
-        } else {
-            res.status(200).json(req.params.id);
-        }
-    });
-});
-
-
-
+require('./races.js')(app, RACES_COLLECTION);
 
 
 /* '/api/classes'
